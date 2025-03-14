50b2523ddde08d1df583b7fef370185963807f9b
John <jjhhoward1738@gmail.com>
2025-03-14 22:40:09 +0300

    adds files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// Function 1: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
